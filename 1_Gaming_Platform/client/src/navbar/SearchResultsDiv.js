2b48e02cfed7a6c740de75fc7449b548b95a7872
Emre Kunduraci <kunduraci2@gmail.com>
2022-12-18 14:01:17 +0100

    Results Div Styling

--- 1_Gaming_Platform/client/src/navbar/SearchResultsDiv.js
+++ 1_Gaming_Platform/client/src/navbar/SearchResultsDiv.js
@@ -1,13 +1,64 @@
 import React from "react";
+import $ from "jquery";
+import Bannerlord from "../assets/imgs/games/1_Bannerlord/1.jpg";
 
 const SearchResultsDiv = () => {
+  const mouseenterHandle = (e) => {
+    const searchImg = $(e.currentTarget).children(".searchImg");
+
+    $(".searchFigure").css({
+      filter: "sepia(60%)",
+    });
+    $(e.currentTarget).css({
+      filter: "contrast(110%)",
+      height: "10rem",
+    });
+    $(searchImg).css({
+      transform: "scale(1.5)",
+    });
+  };
+  const mouseleaveHandle = (e) => {
+    const searchImg = $(e.currentTarget).children(".searchImg");
+
+    $(".searchFigure").css({
+      filter: "none",
+      opacity: "1",
+    });
+    $(e.currentTarget).css({
+      filter: "none",
+      height: "4rem",
+    });
+    $(searchImg).css({
+      transform: "scale(1)",
+    });
+  };
+
   return (
     <div id="searchResultsDiv">
-      {/* <div className="searchResults">Result</div>
-      <div className="searchResults">Result</div>
-      <div className="searchResults">Result</div>
-      <div className="searchResults">Result</div>
-      <div className="searchResults">Result</div> */}
+      <figure
+        className="searchFigure"
+        onMouseEnter={(e) => mouseenterHandle(e)}
+        onMouseLeave={(e) => mouseleaveHandle(e)}
+      >
+        <img src={Bannerlord} alt="" className="searchImg" />
+        <figcaption className="searchCaption">God of War</figcaption>
+      </figure>
+      <figure
+        className="searchFigure"
+        onMouseEnter={(e) => mouseenterHandle(e)}
+        onMouseLeave={(e) => mouseleaveHandle(e)}
+      >
+        <img src={Bannerlord} alt="" className="searchImg" />
+        <figcaption className="searchCaption">God of War</figcaption>
+      </figure>
+      <figure
+        className="searchFigure"
+        onMouseEnter={(e) => mouseenterHandle(e)}
+        onMouseLeave={(e) => mouseleaveHandle(e)}
+      >
+        <img src={Bannerlord} alt="" className="searchImg" />
+        <figcaption className="searchCaption">God of War</figcaption>
+      </figure>
     </div>
   );
 };
